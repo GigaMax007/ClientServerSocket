8782d34f6b23f04b745fc5e6ec105024e9477510
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-04-16 00:39:31 +0500

    link - 39:30 Реализован try ... catch для socket и убраны методы close() т.к. их классы наследуют интерфейс Closeable

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,26 +1,22 @@
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8000);)
-        {
-        System.out.println("Server started!");
+        try (ServerSocket serverSocket = new ServerSocket(8000);) {
+            System.out.println("Server started!");
 
+            try (Socket socket = serverSocket.accept();
+                 BufferedWriter writer = new BufferedWriter(
+                         new OutputStreamWriter(socket.getOutputStream()));) {
 
-        Socket socket = serverSocket.accept();
-        System.out.println("Client connected");
-
-        BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream()));
-
-        writer.write("HELLO FROM SERVER");
-        writer.newLine();
-        writer.flush();
-
-        writer.close();
-        socket.close();
+                writer.write("HELLO FROM SERVER");
+                writer.newLine();
+                writer.flush();
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
