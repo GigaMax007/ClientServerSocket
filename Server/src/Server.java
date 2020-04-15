8a1c57b76efcd8d00304163d5e0007c0fd3467a9
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-04-16 03:09:08 +0500

    link - 1:35:36 Реализованы модуль клиент и сервер, используя написанный class Phone. Код стал чище и лучше

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,31 +1,22 @@
+import kishinskiy.Phone;
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8000);) {
             System.out.println("Server started!");
             while (true)
-            try (Socket socket = serverSocket.accept();
-                 BufferedWriter writer =
-                         new BufferedWriter(
-                                 new OutputStreamWriter(socket.getOutputStream()));
-                 BufferedReader reader =
-                         new BufferedReader(
-                                 new InputStreamReader(
-                                         socket.getInputStream()));
-            ) {
-                String request = reader.readLine();
-                String response = "HELLO FROM SERVER: " + request.length();
-                System.out.println(request);
-                System.out.println(response);
-                writer.write(response);
-                writer.newLine();
-                writer.flush();
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-            }
+                try (Phone phone = new Phone(serverSocket)) {
+                    String request = phone.readLine();
+                    System.out.println("Request: " + request);
+                    String response = (int) (Math.random() * 25 - 10) + "";
+                    phone.writeLine(response);
+                    System.out.println("Response: " + response);
+
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
